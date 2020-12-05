81cb9311a20cbbe929e52aeb45d5547c16c101f0
Noman Sadick <nomansadickjs@gmail.com>
2020-12-05 12:58:38 +0600

    email and passwor not update in firebase

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -16,13 +16,14 @@ const Login = () => {
     }
 
     const [user, setUser] = useState({
-        isSignedIn:false,
-        firstName:'',
-        lastName:'',
-        email:'',
-        password:'',
-        photo:''
-      })
+        isSignedIn: false,
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        photo: ''
+    })
 
     const [newUser, setNewUser] = useState(false)
     const [checked, setChecked] = React.useState(false);
@@ -30,11 +31,13 @@ const Login = () => {
     const handleChange = (e) => {
         setChecked(e.target.checked);
     }
-
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const fbProvider = new firebase.auth.FacebookAuthProvider();
     const handleGoogleSignIn = () => {
-        var provider = new firebase.auth.GoogleAuthProvider();
+        
+        
 
-        firebase.auth().signInWithPopup(provider)
+        firebase.auth().signInWithPopup(googleProvider)
             .then(res => {
                 console.log(res);
             })
@@ -46,102 +49,131 @@ const Login = () => {
             });
     }
 
-    const handleSubmit = () =>{
-
+    const handleFbSignIn = () =>{
+        
+        firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log("Fb user after sign in",user);
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
     }
 
-    const handleBlur = (e) =>{
+
+
+    const handleBlur = (e) => {
         console.log(e.target.name, e.target.value);
-
         let isFieldValid = true;
-        if(e.target.name === 'email'){
-            
-            isFieldValid = /(.+)@(.+){2,}\.(.+){2,}/.test(e.target.value); 
-            
+        if (e.target.name === 'email') {
+
+            isFieldValid = /(.+)@(.+){2,}\.(.+){2,}/.test(e.target.value);
+
         }
 
-        if(e.target.name === 'password'){
-            const isPasswordValid  = e.target.value.length > 8;
-            const passwordHasNumber  = /\d/.test(e.target.value);
+        if (e.target.name === 'password') {
+            const isPasswordValid = e.target.value.length > 8;
+            const passwordHasNumber = /\d/.test(e.target.value);
             isFieldValid = isPasswordValid && passwordHasNumber;
         }
 
 
-        if(e.target.name === 'confirm password'){
-            const isPasswordValid  = e.target.value.length > 8;
-            const passwordHasNumber  = /\d/.test(e.target.value);
+        if (e.target.name === 'confirmPassword') {
+            const isPasswordValid = e.target.value.length > 8;
+            const passwordHasNumber = /\d/.test(e.target.value);
             isFieldValid = isPasswordValid && passwordHasNumber;
         }
 
-        if(isFieldValid){
-            const newUserInfo = {...user};
-             newUserInfo[e.target.name] = e.target.value;
-             setUser(newUserInfo);
+        if (isFieldValid) {
+            const newUserInfo = { ...user };
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
         }
+    }
+
+    const handleSubmit = (e) => {
+        console.log(user.email && user.password);
+        if (user.email && user.password) {
+            console.log('Submitting');
+        }
+        e.preventDefault();
     }
 
     return (
 
         <div>
 
-           <form onSubmit={handleSubmit}>
-           <div className="login-container">
-                {newUser ?
 
-                    <div className="login-field">
-                        <h3>Login</h3>
-                        <TextField className="text" name="email" onBlur={handleBlur} id="standard-basic" type="email" label="User or Email" required/>
-                        <br />
-                        <TextField className="text" name="password" onBlur={handleBlur} id="standard-basic" type="password" label="Password" required/>
-                        <br />
-                        <div className="remember">
-                            <div>
-                                <Checkbox className="checkbox"
-                                    checked={checked}
-                                    onChange={handleChange}
-                                    inputProps={{ 'aria-label': 'primary checkbox' }}
-                                />Remember me
+            <div className="login-container">
+
+                <form onSubmit={handleSubmit}>
+                    {newUser ?
+                        <div className="login-field">
+                            <h3>Login</h3>
+                            <TextField className="text" name="email" onBlur={handleBlur} id="standard-basic" type="email" label="User or Email" required />
+                            <br />
+                            <TextField className="text" name="password" onBlur={handleBlur} id="standard-basic" type="password" label="Password" required />
+                            <br />
+                            <div className="remember">
+                                <div>
+                                    <Checkbox className="checkbox"
+                                        checked={checked}
+                                        onChange={handleChange}
+                                        inputProps={{ 'aria-label': 'primary checkbox' }}
+                                    />Remember me
                             </div>
-                            <div className="forget-password">
-                                <p>Forgot password</p>
+                                <div className="forget-password">
+                                    <p>Forgot password</p>
+                                </div>
                             </div>
+                            <input type="button" value="LOGIN" className="btn" color="secondary" />
+                            <p>Don't have an account? <a href="#login" onClick={() => setNewUser(!newUser)}>Create an account</a></p>
                         </div>
-                        <input type="button" value="LOGIN" className="btn" color="secondary"/>
-                        <p>Don't have an account? <a href="#login" onClick={() => setNewUser(!newUser)}>Create an account</a></p>
-                    </div>
-                    :
+                        :
 
-                    <div className="login-field">
-                        <h3>Create an account</h3>
-                        <TextField className="text" name="firstName" onBlur={handleBlur} id="standard-basic" type="name" label="First Name" required/>
-                        <br />
-                        <TextField className="text" name="lastName" onBlur={handleBlur} id="standard-basic" type="name"  label="Last Name" required/>
-                        <br />
-                        <TextField className="text" name="email" onBlur={handleBlur} id="standard-basic" type="email" label="User or Email" required/>
-                        <br />
-                        <TextField className="text" name="password" onBlur={handleBlur} id="standard-basic" type="password" label="Password" required/>
-                        <br />
-                        <TextField className="text" name="confirm password" onBlur={handleBlur} id="standard-basic" type="password" label="Confirm Password" required/>
-                        <br />
-                        <input type="button" value="CREATE AN ACCOUNT" className="btn" color="secondary"/>
-                        <p>Already have an account? <a href="#login" onClick={() => setNewUser(!newUser)}>Login</a> </p>
-                    </div>
+                        <div className="login-field">
+                            <h3>Create an account</h3>
+                            <TextField className="text" name="firstName" onBlur={handleBlur} id="standard-basic" type="name" label="First Name" required />
+                            <br />
+                            <TextField className="text" name="lastName" onBlur={handleBlur} id="standard-basic" type="name" label="Last Name" required />
+                            <br />
+                            <TextField className="text" name="email" onBlur={handleBlur} id="standard-basic" type="email" label="User or Email" required />
+                            <br />
+                            <TextField className="text" name="password" onBlur={handleBlur} id="standard-basic" type="password" label="Password" required />
+                            <br />
+                            <TextField className="text" name="confirmPassword" onBlur={handleBlur} id="standard-basic" type="password" label="Confirm Password" required />
+                            <br />
+                            <input type="button" value="CREATE AN ACCOUNT" className="btn" color="secondary" />
+                            <p>Already have an account? <a href="#login" onClick={() => setNewUser(!newUser)}>Login</a> </p>
+                        </div>
+                    }
+                </form>
 
-                }
+
             </div>
-                <p className="login-or"><div></div><span>Or</span><div></div></p>
-                <div className="login-with">
-                    <img className="login-image" src={facebook} alt="Facebook" />
-                    <p>Login with Facebook</p>
-                    <div></div>
-                </div>
-                <div className="login-with">
-                    <img className="login-image" src={google} alt="Facebook" />
-                    <button onClick={handleGoogleSignIn}><p>Login with Google</p></button>
-                    <div></div>
-                </div>
+            <p className="login-or"><div></div><span>Or</span><div></div></p>
+            <div className="login-with">
+                <img className="login-image" src={facebook} alt="Facebook" />
+                
+                <button onClick={handleFbSignIn}><p>Login with Facebook</p></button>
+                <div></div>
+            </div>
+            <div className="login-with">
+                <img className="login-image" src={google} alt="Facebook" />
+                <button onClick={handleGoogleSignIn}><p>Login with Google</p></button>
+                <div></div>
+            </div>
 
-           </form>
+
         </div>
     );
 };
